9a29338ec24f3d0483eb98a6896ca2cac95fc5c7
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2026-01-21 14:34:19 +0100

    add: verbose statement to sandboxsession

--- examples/llm_sandbox_min.py
+++ examples/llm_sandbox_min.py
@@ -1,7 +1,7 @@
 from llm_sandbox import SandboxSession
 
 # Create and use a sandbox session
-with SandboxSession(lang="python") as session:
+with SandboxSession(lang="python", verbose=True) as session:
     result = session.run("""
 print("Hello from LLM Sandbox!")
 print("I'm running in a secure container.")
